373100089286f91bb2d33485bcaacd8631b6d172
Swami Sonam Singh <swami.singh@Mac.lan>
2026-07-12 09:29:45 +0530

    navigation lessons

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,34 @@
-export const metadata ={
-    title:'authentication',
-    description:'authentication module'
-}
+'use client'
 
-export default function AuthLayout ({
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import './style.css'
+
+const navLinks = [
+    { name: 'Login', link: '/login' },
+    { name: 'Register', link: '/register' },
+    { name: 'Forgot Passowrd', link: '/forgotPassword' }
+]
+
+export default function AuthLayout({
     children,
-}:{children:React.ReactNode}){
-    return(
-        <html lang="en">
-            <body>
-                {children}
-            </body>
-        </html>
+}: { children: React.ReactNode }) {
+    const pathname = usePathname()
+    return (
+        <div>
+            {navLinks.map((links) => {
+                const isActive = pathname === links.link || (pathname.startsWith(links.link) && links.link !=='/')
+                console.log(isActive)
+                return (
+                    <Link
+                        href={links.link}
+                        key={links.name}
+                        
+                        className={isActive ? "font-bold mr-4 pointer-events-none" : "text-blue-500 m-4"}
+                    >{links.name}</Link>
+                )
+            })}
+            {children}
+        </div>
     )
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next"
+
+type Props ={
+    params : Promise<{productId:string}>
+}
+
+export const generateMetadata = async({
+    params,
+}: Props): Promise<Metadata> => {
+    const id = (await params).productId
+    const title = await new Promise ((resolve)=>{
+        setTimeout(()=>{
+            resolve(`iphone ${id}`)
+        },100)
+    })
+    return{
+        title:`Product ${title}`,
+        description:`this is description of ${title}`
+    }
+}
+
 export default async function ProductDetails({
     params,
 }:{params:Promise<{productId:string}>}){
